dbe7fea3f8c49af695b08ea97c0aec127dfcf3dc
orhun <orhun@archlinux.org>
2021-10-29 13:54:19 +0300

    test: Use customizable sleep time for tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,6 +4,16 @@ use std::env;
 use std::thread;
 use std::time::Duration;
 
+const SLEEP_DURATION: Option<&'static str> = option_env!("SLEEP_DURATION");
+fn sleep() {
+    thread::sleep(Duration::from_millis(
+        SLEEP_DURATION
+            .map(|v| v.parse().ok())
+            .flatten()
+            .unwrap_or(1000),
+    ));
+}
+
 #[test]
 fn test_geolocation_api() -> Result<(), AbstractApiError> {
     let mut abstractapi = AbstractApi::new();
@@ -12,6 +22,7 @@ fn test_geolocation_api() -> Result<(), AbstractApiError> {
         env::var("GEOLOCATION_API_KEY").expect("GEOLOCATION_API_KEY is not set"),
     )?;
 
+    sleep();
     let geolocation: Geolocation = abstractapi.get_geolocation("172.217.19.142")?;
     assert_eq!(
         Some("Google LLC"),
@@ -28,6 +39,7 @@ fn test_holidays_api() -> Result<(), AbstractApiError> {
         env::var("HOLIDAYS_API_KEY").expect("HOLIDAYS_API_KEY is not set"),
     )?;
 
+    sleep();
     let holidays: Holidays = abstractapi.get_holidays("TR", "2021", "10", "29")?;
     assert_eq!("Republic Day", holidays[0].name);
     Ok(())
@@ -41,13 +53,14 @@ fn test_exchange_rates_api() -> Result<(), AbstractApiError> {
         env::var("EXCHANGE_RATES_API_KEY").expect("EXCHANGE_RATES_API_KEY is not set"),
     )?;
 
+    sleep();
     let _ = abstractapi.get_latest_exchange_rates("BTC", Some("USD"))?;
-    thread::sleep(Duration::from_secs(1));
 
+    sleep();
     let rate = abstractapi.get_historical_exchange_rates("BTC", Some("USD"), "2021-01-31")?;
     assert_eq!(Some(33021.639792), rate.exchange_rates.usd);
-    thread::sleep(Duration::from_secs(1));
 
+    sleep();
     let converted_rate =
         abstractapi.convert_currency("USD", "TRY", Some("2021-01-31"), Some(10))?;
     assert_eq!(7.314766, converted_rate.exchange_rate);
@@ -63,6 +76,7 @@ fn test_company_enrichment_api() -> Result<(), AbstractApiError> {
         env::var("COMPANY_ENRICHMENT_API_KEY").expect("COMPANY_ENRICHMENT_API_KEY is not set"),
     )?;
 
+    sleep();
     let company_details: CompanyDetails =
         abstractapi.get_company_details(Some("google.com"), None)?;
     assert_eq!(Some(1998), company_details.year_founded);
@@ -78,11 +92,12 @@ fn test_timezone_api() -> Result<(), AbstractApiError> {
         env::var("TIMEZONE_API_KEY").expect("TIMEZONE_API_KEY is not set"),
     )?;
 
+    sleep();
     let current_time: LocationTime = abstractapi.get_current_time("Ankara")?;
     assert_eq!("Europe/Istanbul", current_time.timezone_location);
     assert_eq!(3., current_time.gmt_offset);
-    thread::sleep(Duration::from_secs(1));
 
+    sleep();
     let converted_time: ConvertedTime = abstractapi.convert_time(
         "Los Angeles,CA",
         "2020-05-01 07:00:00",
